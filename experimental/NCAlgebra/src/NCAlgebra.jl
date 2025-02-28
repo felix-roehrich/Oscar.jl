57b3d75353d080f5fc3485fe75322c6a82ada442
Felix Röhrich <roehrich@art.rwth-aachen.de>
2025-02-28 22:45:15 +0100

    save progres on pbw_algebra

--- experimental/NCAlgebra/src/NCAlgebra.jl
+++ experimental/NCAlgebra/src/NCAlgebra.jl
@@ -1,3 +1,21 @@
 module NCAlgebra
-  include("PBWAlgebra.jl")
+
+using ..Oscar
+
+using AbstractAlgebra.Generic: MPoly
+
+using AbstractAlgebra.PrettyPrinting: Lowercase, LowercaseOff, pretty
+
+import ..Oscar: coeff, coefficient_ring, leading_exponent_vector,
+  leading_term, gen, gens, ngens, tail, zeros
+
+import ..Oscar: add!, mul!, one, one!, zero, zero!, is_zero
+
+include("PBWAlgebra.jl")
+
+export PBWAlg, PBWAlgebraElem
 end
+
+using .NCAlgebra
+
+export PBWAlg, PBWAlgElem
